bc33746ae861279474945e85ff7a13a55c8fb50c
kubaglowczynski <114954433+kubaglowczynski@users.noreply.github.com>
2022-12-03 11:13:15 +0100

    mobile update

--- script.js
+++ script.js
@@ -170,12 +170,3 @@ input.addEventListener('keypress', function(event){
         });
     }
 });
-$(document).ready(function(){
-    document.ontouchmove = function(e){
-        e.preventDefault();
-    }
-});
-input.onfocus = function (){
-    window.scrollTo(0.0);
-    document.body.scrollTop = 0;
-};
